20c71ab8e16fcfe39a5c666dd63f404dea9721b8
Danilo Freitas <danilofs.ti@gmail.com>
2020-09-02 20:31:22 -0300

    ensure LoginRouter returns 401 when invalid credentials are provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -61,4 +61,16 @@ describe('Login Router', () => {
     expect(authUseCaseSpy.email).toEqual(httpRequest.body.email)
     expect(authUseCaseSpy.password).toEqual(httpRequest.body.password)
   })
+
+  test('Should return 401 when invalid credentials are provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'invalid_email@mail.com',
+        password: 'invalid_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(401)
+  })
 })
